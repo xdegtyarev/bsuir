d0de0ad73692f2fa195d5cd31e3f4c7eeb5d0354
Alexander Degytarev <xdegtyarev@gmail.com>
2016-05-08 00:33:33 +0300

    Added new DiffSolver, fixed EQSolver bugs

---
+++ 3/2/MNA/sources/DiffSolver.m
@@ -0,0 +1,90 @@
+function DiffSolver
+syms x y
+e = 0.001
+
+a = 0.5
+m = 2
+
+lb = 0
+ub = 1
+
+h = 0.01
+h2 = h/2
+
+y1 = a*(1-y^2)/((1+m)*x*x+y*y+1)
+
+%%%%% Euler Method
+e1 = EulerMethod(y1,lb,ub,h)
+e2 = EulerMethod(y1,lb,ub,h2)
+%%%%%
+
+%%%%% RungeMethod
+r1 = RungeMethod(y1,lb,ub,h)
+r2 = RungeMethod(y1,lb,ub,h2)
+%%%%%
+
+%%%%% AdamsMethod
+a1 = AdamsMetod(y1,lb,ub,h)
+a2 = AdamsMetod(y1,lb,ub,h2)
+%%%%% 
+
+plot(e1.x,e1.y,e2.x,e2.y,r1.x,r1.y,r2.x,r2.y,a1.x,a1.y,a2.x,a2.y)
+
+end
+
+function res = AdamsMetod(f0,a,b,h)
+    syms x y
+    xk = a
+    yk = 0
+    yp = 0
+    resx = [xk]
+    resy = [yk]
+    while(xk<b)
+        xk = xk+h
+        resx = [resx,xk]
+        yn = double(subs(subs(f0,x,xk),y,yk))
+        yk = yk+h*(1.5*yn-0.5*yp)
+        yp = yn
+        resy = [resy,yk]
+    end
+    res = struct('x',resx,'y',resy)
+    return
+end
+
+function res = RungeMethod(f0,a,b,h)
+    syms x y
+    xk = a
+    yk = 0
+    resx = [xk]
+    resy = [yk]
+    while(xk<b)
+        xk = xk+h
+        resx = [resx,xk]
+        k1 = h*double(subs(subs(f0,x,xk),y,yk))
+        k2 = h*double(subs(subs(f0,x,xk+h*0.5),y,yk+k1/2))
+        k3 = h*double(subs(subs(f0,x,xk+h*0.5),y,yk+k2/2))
+        k4 = h*double(subs(subs(f0,x,xk+h),y,yk+h*k3))
+        yk = yk + (k1+2*k2+2*k3+k4)/6
+        resy = [resy,yk]
+    end
+    res = struct('x',resx,'y',resy)
+    plot(resx,resy)
+    return
+end
+
+function res = EulerMethod(f0,a,b,h)
+    syms x y
+    xk = a
+    yk = 0
+    resx = [xk]
+    resy = [yk]
+    
+    while(xk<b)
+        xk = xk+h
+        resx = [resx,xk]
+        yk = yk+h*double(subs(subs(f0,x,xk),y,yk)) 
+        resy = [resy,yk]
+    end
+    res = struct('x',resx,'y',resy)
+    return
+end
--- 3/2/MNA/sources/EQSolver.m
+++ 3/2/MNA/sources/EQSolver.m
@@ -1,12 +1,11 @@
 function EQSolver()
-    disp('Input formala');
     syms x
     a = '20.2374';
     b = '-131.210';
     c = '-843.923';
-    e ='0.0001';
-    lb = '-10';
-    ub = '10';
+    e = 0.0001;
+    lb = -10;
+    ub = 10;
     f0 = x*x*x + a*x*x + b*x + c;
     
 %%%% Sturm Method
@@ -14,30 +13,32 @@ function EQSolver()
 %%%%     
     
 %%%% root separation
-%      for i=-10:0.1:10
-%       if SturmMethod(f0,i,i+0.1) > 0
-%          disp(i); 
-%       end
-%      end
+       for i=-10:0.1:10
+        if SturmMethod(f0,i,i+0.1) > 0
+           disp(i); 
+        end
+       end
 %%%%
 
 %%%% division by half method
-
+     disp(BisectionMethod(f0,-10,0,0,e));
+     disp(BisectionMethod(f0,0,10,0,e));
 %%%%
      
 %%%% hordes method
-     disp(ChordMethod(f0,-10,0));
-     disp(ChordMethod(f0,0,10));
+      disp(ChordMethod(f0,-10,0,0,e));
+      disp(ChordMethod(f0,0,10,0,e));
 %%%%
 
 %%%% newton method
-     disp(NewtonMethod(f0,-5,-4));
-     disp(NewtonMethod(f0,8,9));   
+      disp(NewtonMethod(f0,-10,0,0,e));
+      disp(NewtonMethod(f0,0,10,0,e));   
 %%%%
 
 %%%% plot
-     figure
-     plot(x,eval(f0),roots,0,'or'); 
+      figure
+      x = lb:0.01:ub
+      plot(x,eval(f0)); 
 %%%%
 end
 
@@ -83,31 +84,53 @@ function res = SturmMethod(f0,lb,ub)
     %here we got a number of roots
 end
 
-function res = ChordMethod(f0,a,b)
+function res = BisectionMethod(f0,a,b,iter,e)
+    syms x
+    xn = a
+    xk = b;
+    xi = a;
+    dx = xk-xn
+    while(abs(xk-xn)>e)
+       iter = iter + 1
+       disp(iter)
+       dx = dx/2
+       xi = xn + dx
+       if(sign(subs(f0,x,xn)) ~= sign(subs(f0,x,xi)))
+           xk = xi
+       else
+           xn = xi
+       end
+    end
+    res = [xi,iter]
+end
+
+function res = ChordMethod(f0,a,b,iter,e)
         %% for ) curve
         syms x
         fa = subs(f0,x,a);
-        fa = subs(f0,x,b);
+        fb = subs(f0,x,b);
         newa = a - fa / (fb - fa) * ( b - a )
-        if abs(newa-a) < eps 
-            res = a
+        iter = iter+1
+        if abs(newa-a) < e
+            res = [a,iter]
             return
         else
-            res = ChordMethod(f0,newa,b)
+            res = ChordMethod(f0,newa,b,iter,e)
             return
         end            
 end
 
-function res = NewtonMethod(f0,a,b)
+function res = NewtonMethod(f0,a,b,iter,e)
         syms x
         fa0 = subs(f0,x,a);
         fa1 = subs(diff(f0),x,a);
         newa = a - fa0 / fa1
-        if abs(newa-a) < eps 
-            res = a
+        iter = iter+1
+        if abs(newa-a) < e 
+            res = [a,iter]
             return
         else
-            res = NewtonMethod(f0,newa,b)
+            res = NewtonMethod(f0,newa,b,iter,e)
             return
         end    
 end
