fd934e0b8830d56f36f10029ae9fe7424b7acbe5
Alexander Degtyarev <xdegtyarev@gmail.com>
2016-11-11 00:15:30 +0300

    moving to freedos

--- 4/1/AVS2/KR1/ipr1.asm
+++ 4/1/AVS2/KR1/ipr1.asm
@@ -1,8 +1,13 @@
 section .data
-toprotectedmsg: db "switching to protected", 10, 0
-inprotectedmsg: db "in protected", 10, 0
+welcomemsg: db "ipr1-degtyarev",10, 0
+torealdmsg: db "switching to real", 10, 0
+inrealdmsg: db "in real", 10, 0
 tonativemsg: db "switching to native", 10, 0
 innativemsg: db "in native", 10, 0
+statmsg: db "%x",10,0
+
+section .bss
+a: resd 1
 
 section .text
 	global main
@@ -14,10 +19,43 @@ main:
 	mov ebp,esp
 
 	;;;;welcome
-	push dword inprotectedmsg
+	push dword welcomemsg
+	call printf
+	add esp, 4; cleaning stack
+
+	;;;;to native mode
+	cli
+	;lgdt [gdtr]	
+	;mov eax, cr0
+	;or eax, 1
+	;mov cr0, eax
+	;jmp 08h:nmain;
+
+;	mov eax, cr0
+;	mov [a], eax
+
+;	push dword [a+4]
+;	push statmsg
+;	call printf
+;	add esp, 8
+
+	push dword inrealdmsg
 	call printf
 	add esp, 4; cleaning stack
 	jmp end;
+
+nmain:
+	push dword innativemsg
+	call printf
+	add esp, 4; cleaning stack
+	jmp end;
+
+rmain:
+	push dword inrealdmsg
+	call printf
+	add esp, 4; cleaning stack
+	jmp end;	
+
 
 end:
 	mov esp, ebp; take down stack frame
