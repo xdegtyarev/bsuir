ccfa60286c4f68873b7c9ed54a6e2e4a9dc48e30
Alexander Degytarev <xdegtyarev@gmail.com>
2017-05-06 01:32:01 +0300

    inherit task

--- 4/2/ITDDP/Homework/ipr2_4_2.js
+++ 4/2/ITDDP/Homework/ipr2_4_2.js
@@ -1,36 +1,40 @@
-
-
-function Task(){
-	if (this instanceof Task) {
+function Task(key){
 		this.name = "unnamed";
 		this.description = "no desc";
 		this.startDate = new Date();
 		this.endDate = new Date();
 		this.subtasks = [];
-
-	}else{
-		return new Task();
-	}
+		console.log("Task .ctor()" + key);
 }
 
-function ActiveTask(progress){
-	if (this instanceof ActiveTask) {
+Task.prototype.toString = function(){
+	return this.name + " " + this.description + " started: " + this.startDate + " ends: " + this.endDate + " has " + this.subtasks.length + " subtasks";
+};
+
+
+function ActiveTask(key){
+		Task.apply(this, arguments);
+		console.log("ATask .ctor()" + key);
 		this.progress = 0;
 		this.isDone = false;
-		ActiveTask.prototype = new Task();
-		ActiveTask.prototype.constructor = ActiveTask;
-		ActiveTask.prototype.GetProgress = function(){
-			return progress;
-		}
-	}else{
-		return new ActiveTask()
-	}
+}
+
+ActiveTask.prototype = new Task();
+ActiveTask.prototype.constructor = ActiveTask;
+
+ActiveTask.prototype.getProgress = function(){
+	return this.progress;
+}
+
+ActiveTask.prototype.toString = function(){
+	var s = Task.prototype.toString.call(this);
+	return s + " progress: " + this.progress;
 }
 
 function ipr2_4_2(){
-	var t1 = new Task();
-	var at1 = new ActiveTask();
-	alert("t1:" + t1);
-	alert("at1:" + at1);
+	var t1 = new Task("t1");
+	var at1 = new ActiveTask("at1");
+	console.log("t1:" + t1 + "is Task:" + (t1 instanceof Task) + "is ActiveTask:" + (t1 instanceof ActiveTask));
+	console.log("at1:" + at1 + "is Task:" + (at1 instanceof Task) + "is ActiveTask:" + (t1 instanceof ActiveTask));
 }
 
