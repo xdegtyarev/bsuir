6ab5d70f0328fa694e3a120e82a062e0e1f0dc24
Alexander Degtyarev <xdegtyarev@gmail.com>
2018-03-23 14:03:59 +0300

    Lab8 init

---
+++ 5/2/TIVP/lab8/lab8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lab8
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello it us lab8, where I try to use Selenium Webdriver!");
+
+        }
+    }
+}
